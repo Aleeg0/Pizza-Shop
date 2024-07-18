f6e14b18c1ad565303cefe3a24e5777c66b86d61
Aleeg0 <Sankafoxe@gmail.com>
2024-07-18 20:25:38 +0300

    update project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import Header from "./Components/Header/Header.tsx";
+
+const App = () => {
+
+  return (
+    <div className="wrapper">
+      <Header/>
+    </div>
+  );
+};
+
+export default App;
