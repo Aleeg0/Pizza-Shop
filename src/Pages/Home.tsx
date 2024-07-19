0ebd78578fda448024aadb0d03d87122e3339da1
Aleeg0 <Sankafoxe@gmail.com>
2024-07-19 22:43:33 +0300

    add pizzas in home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,25 +3,40 @@ import Pizza from "../Components/Pizza";
 import Categories from "../Components/Categories";
 import Searcher from "../Components/Searcher";
 import Selector from "../Components/Selector";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch, RootState} from "../Redux/Store.ts";
+import {fetchPizzas, IPizza} from "../Redux/Slices/PizzasSlice.ts";
+import {useEffect} from "react";
+import {filtersState} from "../Redux/Slices/FiltersSlice.ts";
 
 const Home = () => {
+  const {pizzas} = useSelector((state: RootState) => state.pizzas);
+  const s: filtersState = useSelector((state: RootState) => state.filter);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchPizzas(s));
+  },[])
 
   return (
-    <div>
-      <div className="wrapper">
-        <Header/>
-        <div className="filters">
-          <Categories/>
-          <div className="inputFilters">
-            <Selector/>
-            <Searcher/>
-          </div>
+    <div className="wrapper">
+      <Header/>
+      <div className="filters">
+        <Categories/>
+        <div className="inputFilters">
+          <Selector/>
+          <Searcher/>
         </div>
-        <div className="content">
-          <h1>All pizzas</h1>
-          <div className="container">
-            <Pizza/>
-          </div>
+      </div>
+      <div className="content">
+        <h1>All pizzas</h1>
+        <div className="container">
+          {pizzas.map((pizza: IPizza) =>
+            <Pizza
+              key={pizza.id}
+              {...pizza}
+            />
+          )}
         </div>
       </div>
     </div>
