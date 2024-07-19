93c452cc5bbfb7d3d38cf35c166f976d2305bc0a
Aleeg0 <Sankafoxe@gmail.com>
2024-07-20 00:37:53 +0300

    add logic with search and optimize it wit debounce

--- src/Components/Searcher/index.tsx
+++ src/Components/Searcher/index.tsx
@@ -1,13 +1,34 @@
 import searchImg from "../../assets/Searcher.svg";
 import styles from "../../Styles/Components/_searcher.module.scss"
+import React, {useCallback, useState} from "react";
+import {useDispatch} from "react-redux";
+import {setSearchValue} from "../../Redux/Slices/FiltersSlice.ts";
+import debounce from "lodash.debounce";
+
 
 const Searcher = () => {
+  const [searchText, setSearchText] = useState("");
+  const dispatch = useDispatch();
+
+  const updateSearchValue = useCallback(debounce((value) => {
+      dispatch(setSearchValue(value));
+      },600),
+    []);
+
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+    updateSearchValue(event.target.value);
+  }
+
   return (
     <div className={styles.searcher}>
       <img src={searchImg} alt="sercher"/>
       <input
         type="text"
         placeholder="Search..."
+        value={searchText}
+        onChange={(event) => onChangeInput(event)}
       />
     </div>
   );
