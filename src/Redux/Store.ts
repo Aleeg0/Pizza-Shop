d039384cc5ec087e3acd9a884043152f336f95f5
Aleeg0 <Sankafoxe@gmail.com>
2024-07-19 22:41:57 +0300

    add pizza to store

--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -1,10 +1,12 @@
-import {configureStore} from "@reduxjs/toolkit";
 import filterReducer from "./Slices/FiltersSlice"
+import pizzasReducer from "./Slices/PizzasSlice.ts";
 
 export const store = configureStore({
   reducer: {
     filter: filterReducer,
+    pizzas: pizzasReducer
   },
 })
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
