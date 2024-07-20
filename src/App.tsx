8ffb5105107520580c79684b4fc1e48971bac202
Aleeg0 <Sankafoxe@gmail.com>
2024-07-21 00:32:30 +0300

    wrapped with react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Home from "./Pages/Home.tsx";
+import {Route, Routes} from "react-router";
+import Cart from "./Pages/Cart.tsx";
 
 const App = () => {
 
   return (
-    <>
-      <Home/>
-    </>
+    <div className="wrapper">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="cart" element={<Cart/>} />
+      </Routes>
+    </div>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import App from "./App.tsx";
 import "./Styles/index.css"
 import {Provider} from "react-redux";
 import {store} from "./Redux/Store.ts";
+import {BrowserRouter as Router} from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Router>
     <Provider store={store}>
       <App/>
-    </Provider>,
+    </Provider>
+  </Router>,
 )
