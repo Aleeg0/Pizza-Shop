a9c87c9f9572432a66e3ad5649cc0f7c0fb07376
Aleeg0 <Sankafoxe@gmail.com>
2024-07-20 17:52:57 +0300

    move pizza interface to types

---
+++ src/Redux/Types/IPizzas.ts
@@ -0,0 +1,10 @@
+export interface IPizza {
+    id: number,
+    title: string,
+    imgURL: string,
+    types: number[],
+    sizes: number[],
+    price: number,
+    category: number,
+    rating: number
+}
