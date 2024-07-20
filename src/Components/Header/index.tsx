9ba4fbf73e32cb8e8ab7af28c52fdc38282acf49
Aleeg0 <Sankafoxe@gmail.com>
2024-07-21 00:36:49 +0300

    wrapped with links for react-router

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -2,27 +2,32 @@ import {FC} from 'react';
 import Logo from "/logo.svg";
 import styles from "../../Styles/Components/_header.module.scss";
 import CartSvg from "../SVGS/cartSvg.tsx";
+import {Link} from "react-router-dom";
 
 const Header: FC = () => {
   return (
     <div className={styles.header}>
       <div className={styles.content}>
         <div className={styles.info}>
-          <img src={Logo} alt="appLogo"/>
-          <div className={styles.title}>
-            <h1>Pizza shop</h1>
-            <p>the most delicious pizza in the universe</p>
-          </div>
+          <Link to="/">
+            <img src={Logo} alt="appLogo"/>
+            <div className={styles.title}>
+              <h1>Pizza shop</h1>
+              <p>the most delicious pizza in the universe</p>
+            </div>
+          </Link>
         </div>
-        <button
-          type="button"
-          className={styles.cartButton}
-        >
-          <p>100 $</p>
-          <span/>
-          <CartSvg/>
-          <p>3</p>
-        </button>
+        <Link to="cart">
+          <button
+            type="button"
+            className={styles.cartButton}
+          >
+            <p>100 $</p>
+            <span/>
+            <CartSvg/>
+            <p>3</p>
+          </button>
+        </Link>
         </div>
       <hr className={styles.splitter}/>
     </div>
