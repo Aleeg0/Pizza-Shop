d54858475493cdd304d874eae53355e00b9cf279
Aleeg0 <Sankafoxe@gmail.com>
2024-07-21 20:04:58 +0300

    rename addCartSvg

--- src/Components/SVGS/AddToCartSvg.tsx
+++ src/Components/SVGS/AddToCartSvg.tsx
@@ -1,4 +1,4 @@
-const AddCartSvg = () => {
+const AddToCartSvg = () => {
   return (
     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
@@ -8,4 +8,4 @@ const AddCartSvg = () => {
   );
 };
 
-export default AddCartSvg;
+export default AddToCartSvg;
