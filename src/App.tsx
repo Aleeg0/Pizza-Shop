2b0e2a045240492cf113191e61e4e1df33ed719e
Aleeg0 <Sankafoxe@gmail.com>
2024-07-22 18:18:59 +0300

    add NotFound Page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Home from "./Pages/Home.tsx";
 import {Route, Routes} from "react-router";
 import Cart from "./Pages/Cart.tsx";
+import NotFound from "./Pages/NotFound.tsx";
 
 const App = () => {
 
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="cart" element={<Cart/>} />
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </div>
   );
