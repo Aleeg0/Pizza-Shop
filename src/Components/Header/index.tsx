14aa454cfc6b8ff1b3ab73773a88c0d939682a47
Aleeg0 <Sankafoxe@gmail.com>
2024-07-22 15:32:47 +0300

    improve UI

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -2,12 +2,13 @@ import {FC} from 'react';
 import Logo from "/logo.svg";
 import styles from "../../Styles/Components/_header.module.scss";
 import CartSvg from "../SVGS/cartSvg.tsx";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import {useSelector} from "react-redux";
 import {RootState} from "../../Redux/Store.ts";
 
 const Header: FC = () => {
   const {totalSum, totalCount} = useSelector((state: RootState) => state.cart)
+  const path = useLocation();
 
   return (
     <div className={styles.header}>
@@ -21,17 +22,19 @@ const Header: FC = () => {
             </div>
           </Link>
         </div>
-        <Link to="cart">
-          <button
-            type="button"
-            className={styles.cartButton}
-          >
-            <p>{totalSum.toFixed(2)} $</p>
-            <span/>
-            <CartSvg/>
-            <p>{totalCount}</p>
-          </button>
-        </Link>
+        {!path.pathname.includes("cart") &&
+            <Link to="cart">
+                <button
+                    type="button"
+                    className={styles.cartButton}
+                >
+                    <p>{totalSum.toFixed(2)} $</p>
+                    <span/>
+                    <CartSvg/>
+                    <p>{totalCount}</p>
+                </button>
+            </Link>
+        }
         </div>
       <hr className={styles.splitter}/>
     </div>
