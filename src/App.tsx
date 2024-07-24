82c8c8b25637f6757dcde27e27e741187cb693a7
Aleeg0 <Sankafoxe@gmail.com>
2024-07-24 13:31:10 +0300

    make app routes more clear

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import Home from "./Pages/Home.tsx";
 import {Route, Routes} from "react-router";
 import Cart from "./Pages/Cart.tsx";
 import NotFound from "./Pages/NotFound.tsx";
+import PopUpPizza from "./Pages/PopUpPizza.tsx";
+import AppLayout from "./layouts/AppLayout.tsx";
 
 const App = () => {
 
   return (
-    <div className="wrapper">
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="cart" element={<Cart/>} />
+    <Routes>
+      <Route path="" element={<AppLayout/>}>
+        <Route path="" element={<Home/>}>
+          <Route path="id/:id" element={<PopUpPizza/>}/>
+        </Route>
+        <Route path="cart" element={<Cart/>}/>
         <Route path="*" element={<NotFound/>}/>
-      </Routes>
-    </div>
+      </Route>
+    </Routes>
   );
 };
 
