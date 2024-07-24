4704b2d8f29927bef46100a9698506d99ded3206
Aleeg0 <Sankafoxe@gmail.com>
2024-07-24 21:15:28 +0300

    fix basename

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import {BrowserRouter as Router} from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Router>
+  <Router basename={"Pizza-Shop"}>
     <Provider store={store}>
       <App/>
     </Provider>
